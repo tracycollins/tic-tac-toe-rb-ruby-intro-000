13440535e9b65f0ef6f029fa00f251ba701e1e05
Tracy Collins <tc@threeceemedia.com>
2020-02-18 03:41:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,7 +57,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  player = current_player
+  player = current_player(board)
   if valid_move?(board, index, player)
     move(board, index)
     display_board(board)
