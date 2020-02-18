728cd6b517b0c587cd91b1ab44224eb0eb4bdf61
Tracy Collins <tc@threeceemedia.com>
2020-02-18 03:41:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,7 +58,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   player = current_player(board)
-  if valid_move?(board, index, player)
+  if valid_move?(board, index)
     move(board, index)
     display_board(board)
   else
